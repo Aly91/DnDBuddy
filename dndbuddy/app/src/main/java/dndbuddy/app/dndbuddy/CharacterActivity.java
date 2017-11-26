44270446c41c4aa8ddc209c533424d1e9f6e6c58
Aly91 <alyson.smith@student.dit.ie>
2017-11-26 12:48:50 +0000

    implemented main menu functionality and currently implementing database logic

--- dndbuddy/app/src/main/java/dndbuddy/app/dndbuddy/CharacterActivity.java
+++ dndbuddy/app/src/main/java/dndbuddy/app/dndbuddy/CharacterActivity.java
@@ -3,15 +3,28 @@ package dndbuddy.app.dndbuddy;
 import android.appwidget.AppWidgetProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CharacterActivity extends AppCompatActivity {
 
     AppDB db;
 
+   private TextView name;
+   private TextView race;
+    private TextView cClass;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
+
+        name = (TextView) findViewById(R.id.displayName);
+        race = (TextView) findViewById(R.id.displayRace);
+        cClass = (TextView) findViewById(R.id.displayclass);
 
         db = new AppDB(this);
 
